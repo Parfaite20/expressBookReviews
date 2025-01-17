24b2c8705d590c4a116a80738358043c8ca87bc4
Parfaite <parfaitehounkpatchi@gmail.com>
2025-01-17 19:29:48 +0100

    commit3

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,20 +3,44 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+  { username: "Parfaite", password: "password123" } // Ajout de l'utilisateur "Parfaite"
+];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+
+return users.some(user => user.username === username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+
+const user = users.find(user => user.username === username);
+  return user && user.password === password;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // return res.status(300).json({message: "Yet to be implemented"});
+
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  if (isValid(username) && authenticatedUser(username, password)) {
+
+    const accessToken = jwt.sign({ username }, "access", { expiresIn: '1h' });
+
+    req.session.authorization = { accessToken };
+
+    return res.status(200).json("Customer successfully logged in" );
+  } else {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
 });
 
 // Add a book review
